6720fb9b3f3a1d4448aa6cb0624d7789782262e9
XingOfficial <3826270809@qq.com>
2026-09-07 14:26:22 +0800

    版本号升级到 v1.0.2

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -43,7 +43,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
           ListTile(
             leading: const Icon(Icons.info_outline),
             title: const Text('关于'),
-            subtitle: const Text('云笔记 v1.0.1'),
+            subtitle: const Text('云笔记 v1.0.2'),
             onTap: () {
               Navigator.pushNamed(context, '/about');
             },
